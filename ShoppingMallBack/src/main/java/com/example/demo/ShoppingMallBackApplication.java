19309584b26bc56a02db594f463e5637cd14d590
Shopping-prj <ksk29433@gmail.com>
2025-09-05 17:34:29 +0900

    default

---
+++ ShoppingMallBack/src/main/java/com/example/demo/ShoppingMallBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMallBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingMallBackApplication.class, args);
+	}
+
+}
